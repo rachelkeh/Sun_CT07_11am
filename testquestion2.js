ee74a3b7b63f06c787f1aa3ad575fcc43db6768c
rachelkeh <70307497+rachelkeh@users.noreply.github.com>
2026-09-13 03:24:46 +0000

    Sep 13, 2026, 3:24 AM

--- testquestion2.js
+++ testquestion2.js
@@ -4,7 +4,7 @@ let floor;
 function setup() {
     createCanvas(600, 400);
     world.gravity.y = 2;
-    floor = new Sprite(0, 350, )
+    floor = new Sprite(0, 370, 600,30);
 }
 
 function draw() {
