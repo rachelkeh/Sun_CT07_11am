d7626e1c4a4f41f1bdebd5639109634771fddca7
rachelkeh <70307497+rachelkeh@users.noreply.github.com>
2026-09-13 03:19:44 +0000

    Sep 13, 2026, 3:19 AM

--- testquestion1.js
+++ testquestion1.js
@@ -2,12 +2,11 @@ let circle;
 
 function setup() {
     createCanvas(600, 400);
-    background("white");
     world.gravity.y = 2;
 }
 
 function draw() {
-
+    background("")
     if (mouse.presses()) {
         circle = new Sprite(mouse.x, mouse.y, 30);
     }
